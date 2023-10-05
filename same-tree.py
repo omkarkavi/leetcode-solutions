578c1914758cf4d1b5e59567efdbfbb4dd91833c
Omkar K <okaviom@gmail.com>
2023-10-04 20:35:20 -0700

    added solutions for binary tree

---
+++ same-tree.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p==None and q==None:
+            return True
+        if p==None or q==None:
+            return False
+        if p.val==q.val:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
