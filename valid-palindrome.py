9eba8bc584b117806abc56ed2c322452f05ea5aa
Omkar K <okaviom@gmail.com>
2023-10-05 16:56:44 -0700

    Valid pallindrome

---
+++ valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        
+            
+        s = [i for i in s.lower() if i.isalnum()]
+        return s==s[::-1]
