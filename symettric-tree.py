889eb211944093a539963dd99f09d2cc70b369fe
Omkar K <okaviom@gmail.com>
2023-10-04 20:55:03 -0700

    remove duplicates

---
+++ symettric-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+        return self.isSameTreeReverse(root.left,root.right) 
+        
+        
+        
+    def isSameTreeReverse(self, p, q):
+        if p is None and q is None:
+            return True
+
+        if p and q and p.val == q.val:
+            return self.isSameTreeReverse(p.left, q.right) and self.isSameTreeReverse(p.right, q.left)
+
+        return False
